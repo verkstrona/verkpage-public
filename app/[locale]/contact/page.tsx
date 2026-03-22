6c056198bdeb3eced43ce4a0ee960a9455c9ad73
RobFrontend <robert.grabowski97@gmail.com>
2026-03-22 21:43:33 +0100

    first

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,7 +1,10 @@
+import { useTranslations } from "next-intl";
+
 export default function ContactPage() {
+  const t = useTranslations("ContactPage");
   return (
     <div className="h-screen grid place-items-center">
-      <h1 className="text-[-var(--font-color)]">Contact</h1>
+      <h1 className="text-[-var(--font-color)]">{t("title")}</h1>
     </div>
   );
 }
