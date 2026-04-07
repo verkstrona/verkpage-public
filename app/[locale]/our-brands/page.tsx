60285cccc6f1abc533fc2b9eb673e67d6d1abad8
RobFrontend <robert.grabowski97@gmail.com>
2026-04-07 20:05:39 +0200

    first

--- app/[locale]/our-brands/page.tsx
+++ app/[locale]/our-brands/page.tsx
@@ -1,10 +1,143 @@
+import RevealingSections from "@/app/components/revealingSections";
 import { useTranslations } from "next-intl";
+import KV from "@/public/brandskv.png";
+import KV2 from "@/public/brandskv2.png";
+import KVverk from "@/public/LogoVerk.png";
+import KVdivo from "@/public/LogoDivo.png";
+import KVopex from "@/public/LogoOpex.png";
+import KVprzyjaciele from "@/public/LogoPrzyjaciele.png";
+
+import Image from "next/image";
+import Link from "next/link";
 
 export default function BrandsPage() {
   const t = useTranslations("BrandsPage");
   return (
-    <section className="h-screen grid place-items-center">
-      <h1 className="text-[-var(--font-color)]">{t("title")}</h1>
-    </section>
+    <>
+      <section className="hero-section-about relative">
+        <div className="text-box flex flex-col gap-2 container">
+          <h1 className="text-center uppercase pb-6">{t("title")}</h1>
+          <h2 className="text-center">{t("subheading")}</h2>
+          <RevealingSections goinUp={false}>
+            <p>{t("description1")}</p>
+          </RevealingSections>
+        </div>
+        <Image
+          alt="Key Visual"
+          src={KV}
+          fill
+          quality={100}
+          objectFit="cover"
+          className="z-[-1] max-md:object-left object-center"
+        />
+      </section>
+      <section className="disclaimer-section">
+        <div className="container brands-disclaimer">
+          <p className="pb-4">{t("disclaimer1")}</p>
+          <p>{t("disclaimer2")}</p>
+        </div>
+      </section>
+      <section className="brands-section">
+        <div className="container">
+          <h2 className="opacity-75 pb-12 text-center max-[897px]:pb-16">
+            {t("brandsListHeading")}
+          </h2>
+          <div className="grid gap-24 px-4 items-center justify-items-center">
+            <div>
+              <h3 className="opacity-90 pb-8 max-[897px]:hidden">
+                {t("Verkheading")}
+              </h3>
+              <div className="brands-box">
+                <div>
+                  <Image alt="map" src={KVverk} />
+                </div>
+                <div className="flex flex-col gap-4 brands-textbox">
+                  <h3 className="opacity-90 min-[897px]:hidden pb-4">
+                    {t("Verkheading")}
+                  </h3>
+                  <p>{t("Verkdescription1")}</p>
+                  <p>{t("Verkdescription2")}</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h3 className="opacity-90 pb-8 max-[897px]:hidden ">
+                {t("Divoheading")}
+              </h3>
+              <div className="brands-box">
+                <div>
+                  <Image alt="map" src={KVdivo} />
+                </div>
+                <div className="flex flex-col gap-4 brands-textbox">
+                  <h3 className="opacity-90 min-[897px]:hidden pb-4">
+                    {t("Divoheading")}
+                  </h3>
+                  <p>{t("Divodescription1")}</p>
+                  <p>{t("Divodescription2")}</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h3 className="opacity-90 pb-8 max-[897px]:hidden">
+                {t("Opexheading")}
+              </h3>
+              <div className="brands-box">
+                <div>
+                  <Image alt="map" src={KVopex} />
+                </div>
+                <div className="flex flex-col gap-4 brands-textbox">
+                  <h3 className="opacity-90 min-[897px]:hidden  pb-4">
+                    {t("Opexheading")}
+                  </h3>
+                  <p>{t("Opexdescription1")}</p>
+                  <p>{t("Opexdescription2")}</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <h3 className="opacity-90 pb-8 max-[897px]:hidden ">
+                {t("Przyjacieleheading")}
+              </h3>
+              <div className="brands-box">
+                <div>
+                  <Image alt="map" src={KVprzyjaciele} />
+                </div>
+                <div className="flex flex-col gap-4 brands-textbox">
+                  <h3 className="opacity-90 min-[897px]:hidden pb-4">
+                    {t("Przyjacieleheading")}
+                  </h3>
+                  <p>{t("Przyjaciedescription1")}</p>
+                  <p>{t("Przyjaciedescription2")}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-16 pt-24 justify-center w-full items-center max-[897px]:flex-col">
+            <Link
+              className="btn-primary"
+              href="https://verk.sklep.pl"
+              target="_blank"
+            >
+              {t("shopLink")}
+            </Link>
+            <Link
+              className="btn-secondary"
+              href="https://internetowa-hurtownia.pl/main-cze.html"
+              target="_blank"
+            >
+              {t("wholesaleLink")}
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className="brandsdev-section">
+        <div className="container text-center">
+          <h2 className="opacity-75 pb-12">{t("brandsDevelopmentHeading")}</h2>
+          <p>{t("brandsDevelopmentDescription1")}</p>
+          <p className="pb-8">{t("brandsDevelopmentDescription2")}</p>
+          <Image src={KV2} alt="kv" className="w-full h-auto" />
+        </div>
+      </section>
+    </>
   );
 }
